ae95dfe4dbcad050faadf0f69f6e5922081e022e
vedant <vedant.m.srinivas@gmail.com>
2021-09-20 16:40:44 -0700

    strengthen the code for watchdog and add ngrok restart on wifi reconnection

--- scripts/watchdog.sh
+++ scripts/watchdog.sh
@@ -1,65 +1,75 @@
 #!/bin/bash
 light_red='\e[1;91m%s\e[0m\n'                     
 light_green='\e[1;92m%s\e[0m\n'     
-ip_address='192.168.1.13'
-ping_url="www.google.com"
+camera_ip_address='192.168.1.13'
+locahost_ip_address='192.168.1.1'
+ping_url='debian.org'
 restart_network=false
 restart_service=false
+network_status=true
 
 check_network_status()
 {
-  #Check eh0 and connectivity to IP camera              
-  ping -c 2 -q $ip_address              
+  #Check eh0 and connectivity to IP camera 
+  network_on=0             
+  ping -c 2 -q $camera_ip_address > /dev/null             
   if [ "$?" -eq 0 ]; then                           
     printf "$light_green" "[ IP Camera Connected ]"
   else                                              
     printf "$light_red" "[ IP Camera Disconnected ]"
-    restart_network=true    
+    network_on=1    
   fi
 
 
   #Check wifi network connectivity
-  ping -c 2 -q $ping_url    
+  ping -c 2 -q $ping_url > /dev/null
   if [ "$?" -eq 0 ]; then                           
     printf "$light_green" "[ WiFi Connected ]"
   else                                              
     printf "$light_red" "[ WiFi Disconnected ]"     
-    restart_network=true
+    network_on=2
   fi
-  if [ $restart_network == true ]; then
-    return 1
-  fi
-  return 0
+  return $network_on
 }
 
-reset_network()
+reset_ip_network()
 {
   #restart network manager if required
+  /sbin/ifconfig eth0 $locahost_ip_address netmask 255.255.255.0
+}
+
+reset_wifi_network()
+{
   systemctl stop NetworkManager
-  /sbin/ifconfig eth0 192.168.1.1 netmask 255.255.255.0
   systemctl restart NetworkManager
   systemctl start networking.service
 }
 
-check_network_status
-reset_network_flag=$?
-if [ $reset_network_flag == 1 ]; then
-  printf "$light_green" "[ Network Reset ]"
-  reset_network
-  #WAIT FOR A MINUTE
-  sleep 30s
-else
-  printf "$light_green" "[ Network Connected ]"
-  exit
+#retry to connect three times
+network_reset_happened=false
+for i in 1 2 3
+do
+  check_network_status
+  network_on=$?
+  if [ $network_on == 1 ]; then
+    printf "$light_green" "[ Resetting IP Camera... ]"
+    reset_ip_network   
+  elif [ $network_on == 2 ]; then
+    printf "$light_green" "[ Resetting WiFi Network... ]"
+    reset_wifi_network
+    network_reset_happened=true
+    #Wait for network to come back online
+    sleep 60s
+  else
+    printf "$light_green" "[ Network Connected ]"
+    exit
+  fi
+done
+
+if [ $network_reset_happened ]; then
+  systemctl stop ngrok.service
+  systemctl start ngrok.service
 fi
 
-
-
-check_network_status
-reset_network_flag_after=$?
-
-#If the flag went from bad to good, that means most likely eqwis code is corrupted, so restart the eqwis service
-if [ $reset_network_flag != $reset_network_flag_after ]; then
-  printf "$light_green" "[ Restart Eqwis Service ]"
-fi
-#Check if eqwis code is running without errors
+#If the code comes here, it means the network was not 
+#connected even after three attemps and we will need to reboot.
