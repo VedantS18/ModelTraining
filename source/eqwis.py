58ba417ef597932e95e23aab4c1efb4ddd34267a
vedant <vedant.m.srinivas@gmail.com>
2021-10-10 21:16:33 -0700

    Move text height by 5 pixels

--- source/eqwis.py
+++ source/eqwis.py
@@ -99,7 +99,7 @@ class ObjectDetection:
                 x1, y1, x2, y2 = int(row[0]*x_shape), int(row[1]*y_shape), int(row[2]*x_shape), int(row[3]*y_shape)
                 bgr = (0, 255, 0)
                 cv2.rectangle(frame, (x1, y1), (x2, y2), bgr, 2)
-                cv2.putText(frame, self.class_to_label(labels[i]) + str(conf), (x1, y1), cv2.FONT_HERSHEY_SIMPLEX, 0.9, bgr, 2)
+                cv2.putText(frame, self.class_to_label(labels[i]) + str(conf), (x1, y1-5), cv2.FONT_HERSHEY_SIMPLEX, 0.9, bgr, 2)
 
         return frame
 
